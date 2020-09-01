ad35a470ec29d922eef89060b45a0891f7b6b751
Scott Wilson <scottwilson1020@gmail.com>
2020-09-01 21:06:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,5 +5,16 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  def initialize (id, name, grade)
+    @name = name
+    @grade = grade
+    @id = nill
+    
+  end 
+
+
+def self.create_table
+  
+end 
 
 end
