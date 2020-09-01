7ebcb7c5e8753cdf175975b487b6ea937554903d
Scott Wilson <scottwilson1020@gmail.com>
2020-09-01 21:26:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,10 +45,11 @@ def self.create_table
     end
   end 
     
-  def self.create 
-    
-    
-  end 
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
   
     
 end
